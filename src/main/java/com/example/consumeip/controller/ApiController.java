c2123038ca791dcdf08d8020de005e2c13c5b991
muradiodev <muradace@gmail.com>
2022-10-09 19:41:37 +0200

    added sl4j

--- src/main/java/com/example/consumeip/controller/ApiController.java
+++ src/main/java/com/example/consumeip/controller/ApiController.java
@@ -4,6 +4,7 @@ import com.example.consumeip.model.ResponseData;
 import com.example.consumeip.model.ResultData;
 import com.example.consumeip.services.TeamServices;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,12 +12,14 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class ApiController {
 
     private final TeamServices teamServices;
 
     @GetMapping("/player")
     public ResponseData<List<ResultData>> getPlayers() {
+        log.info("getPlayers() is running");
         return teamServices.getPlayers();
     }
 }
